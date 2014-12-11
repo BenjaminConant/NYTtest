c9ab6c7ea16822d9d72600684813831ee2ab507b
Benjamin Conant <conantbenjamin@gmail.com>
2014-12-10 23:14:10 -0500

    three question types working  .. all image based .. answers are stil dumb

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,182 +1,235 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name quizApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the quizApp
- */
+* @ngdoc function
+* @name quizApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the quizApp
+*/
 angular.module('quizApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+.controller('MainCtrl', function ($scope, $http) {
+  $scope.awesomeThings = [
+  'HTML5 Boilerplate',
+  'AngularJS',
+  'Karma'
+  ];
 
-    // http://www.nytimes.com/
-    // before images
-    // pub date is in Timestamp (YYYY-MM-DD)
-    // jfkdls;afjkdls;afjkdlsa;fjkdlsa;fjkdlsa;f
-    // image questions (show image and ask what article it is from)
-    // city type question (show article and ask waht city it was written in)
-    // columist type question (show a headline and ask what columist wrote it)
-    //     -- harder type would be a random sentence
-    // resturant type question this article was about which NYC resturant
-    // theaters.
-    // question ideas
-    // ====================================================
-    // ====================================================
-    // * catagory keyword => article title *
-    // author to catagory keyword
-    //
-
-
-    $scope.quizObject = {
-      date: "",
-      questions: {},
-      playsInfo: {}
-    }
-    $scope.imageQuestions = [];
-    $scope.sentenceFinisherQuestions = [];
-    $scope.keywordTitleQuestions = [];
-    $scope.keywordTitleQuestionsAnswersArray = [];
-    $scope.imageQuestionAnswersArray = [];
-    $scope.sentenceFinisherQuestionAnswersArray = [];
-    $scope.showApiCall = [];
-
-    function ImageQuestion(question, image, answerChoices, questionType, showQuestion){
-      this.question = question;
-      this.image = image;
-      this.answerChoices = [];
-      this.questionType = questionType;
-      this.showQuestion = showQuestion;
-    }
-
-    function SentenceFinisherQuestion(question, answerChoices, questionType, showQuestion) {
-      this.question = question;
-      this.answerChoices = [];
-      this.questionType = questionType;
-      this.showQuestion = showQuestion;
-    }
-
-    function KeywordTitleQuestion(question, answerChoices, questionType, showQuestion) {
-      this.question = question;
-      this.answerChoices = [];
-      this.questionType = questionType;
-      this.showQuestion = showQuestion;
-    }
+  // http://www.nytimes.com/
+  // before images
+  // pub date is in Timestamp (YYYY-MM-DD)
+  // jfkdls;afjkdls;afjkdlsa;fjkdlsa;fjkdlsa;f
+  // image questions (show image and ask what article it is from)
+  // city type question (show article and ask waht city it was written in)
+  // columist type question (show a headline and ask what columist wrote it)
+  //     -- harder type would be a random sentence
+  // resturant type question this article was about which NYC resturant
+  // theaters.
+  // question ideas
+  // ====================================================
+  // ====================================================
+  // * catagory keyword => article title *
+  // author to catagory keyword
+  //
+  // https://www.polymer-project.org/apps/topeka/ -open source front end
 
 
-    $scope.NYTapiKey = "bdcebec4874a5076dbaaa7f2a5f0db3b:3:70140904";
-    $scope.apiCallNYT = function(resultsPageNumber) {
+  $scope.quizObject = {
+    date: "",
+    questions: [],
+    playsInfo: {}
+  }
+
+  $scope.imageQuestions = [];
+  $scope.articleTitlesArray = [];
+  $scope.articleKeyWordsArray = [];
+  $scope.articleSnippetsArray = [];
+  $scope.articleImagesArray = [];
+
+  function ImageQuestion(question, image, answerChoices, questionType, showQuestion, difficultyLevel){
+    this.question = question;
+    this.image = image;
+    this.answerChoices = [];
+    this.questionType = questionType;
+    this.showQuestion = showQuestion;
+    this.difficultyLevel = difficultyLevel;
+  }
+
+
+  $scope.NYTapiKey = "bdcebec4874a5076dbaaa7f2a5f0db3b:3:70140904";
+  $scope.apiCallNYT = function(resultsPageNumber) {
     $http.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=source:("The New York Times") AND pub_date:("2014-12-3")' + '&page=' + resultsPageNumber + '&api-key=' + $scope.NYTapiKey)
-              .success(function(results, status, headers, jqXHR) {
+    .success(function(results, status, headers, jqXHR) {
 
-        // start doing stuff with results
-          //create a docs array from results object ... docs array is the part of  the respoce objecet we actulaly care about
-                  var resultsObject = results;
-                  var docsArray = resultsObject['response']['docs'];
+      var resultsObject = results;
+      var docsArray = resultsObject['response']['docs'];
+      console.log(docsArray);
+      // create the articleTitlesArray - used for the answers to easy questions.
+      docsArray.forEach(function(document) {
+        if (document.headline.main && document.headline.main !== "") {
+          $scope.articleTitlesArray.push(document.headline.main);
+        }
+      });
 
+      // create the articleKeyWordsArray - used for the answers to medium questions
+      // this could possibly be improved by removing anything thats inside ( ) from the key words string
+      docsArray.forEach(function(document) {
+        if (document.keywords.length > 0) {
+          var keywordsString = "";
+          document.keywords.forEach(function(keywordObject) {
+            if (keywordObject.value) {
+              keywordsString += keywordObject.value + "," + " ";
+            }
+          });
+          keywordsString = keywordsString.trim();
+          keywordsString = keywordsString.substring(0, keywordsString.length-1);
+          $scope.articleKeyWordsArray.push(keywordsString);
+        }
+      });
 
-
-        // [A-Z]([a-z]+|\.)(?:\s+[A-Z]([a-z]+|\.))*(?:\s+[a-z][a-z\-]+){0,2}\s+[A-Z]([a-z]+|\.)
-
-
-        // generate image questions
-            // generate possible answers array
-                  docsArray.forEach(function(document) {
-                    if (document.headline.main && document.multimedia.length > 0 && document.headline.main !== "") {
-
-                      $scope.imageQuestionAnswersArray.push(document.headline.main);
-
-                      }
-                    });
-
-            // generate ImageQuestions and KeyWord => KeywordTitleQuestions
-
-                  docsArray.forEach(function(doc) {
-                    if (doc.multimedia.length > 0 && doc.keywords.length > 0) {
-
-                      var newImageQuestion = new ImageQuestion();
-                      newImageQuestion.question = "The image above most likely goes with which article?";
-                      newImageQuestion.image = "http://www.nytimes.com/" + doc.multimedia[0].url;
-                      newImageQuestion.answerChoices.push({'value': doc.headline.main, 'isCorrect': true});
-                      newImageQuestion.questionType = "image";
-                      newImageQuestion.showQuestion = true;
-
-                      var newKeywordTitleQuestion = new KeywordTitleQuestion();
-                      newKeywordTitleQuestion.question = "Which of the the articles below most closly match this topic: " + doc.keywords[0].value;
-                      newKeywordTitleQuestion.answerChoices.push({'value': doc.headline.main, 'isCorrect': true});
-                      newKeywordTitleQuestion.questionType = "image";
-                      newKeywordTitleQuestion.showQuestion = true;
-
-                      for (var i = 0; i < 4; i++) {
-                        var randomIndex = Math.floor((Math.random() * (docsArray.length -1)) + 0);
-                        if (newImageQuestion.answerChoices[0].value !== $scope.imageQuestionAnswersArray[randomIndex]) {
-                          newImageQuestion.answerChoices.push({'value': $scope.imageQuestionAnswersArray[randomIndex], 'isCorrect': false});
-                          newKeywordTitleQuestion.answerChoices.push({'value': $scope.imageQuestionAnswersArray[randomIndex], 'isCorrect': false});
-                        }
-                      }
-                      $scope.imageQuestions.push(newImageQuestion);
-                      $scope.keywordTitleQuestions.push(newKeywordTitleQuestion);
-
-                      console.log($scope.keywordTitleQuestions);
-                    }
-
-
-                  });
+      // create the articleSnippetsArray and articleImagesArray - used for the questions for hard questions
+      docsArray.forEach(function(document) {
+        if (document.snippet && document.multimedia.length > 0) {
+          $scope.articleSnippetsArray.push(document.snippet);
+          $scope.articleImagesArray.push("http://www.nytimes.com/" + document.multimedia[0].url);
+        }
+      });
 
 
 
+      // create the Easy and Medium Questions
+      docsArray.forEach(function(doc, index) {
+        // easy
+        if (doc.multimedia.length > 0) {
+
+          var newEasyImageQuestion = new ImageQuestion();
+          newEasyImageQuestion.question = "The image above most likely goes with which article?";
+          newEasyImageQuestion.image = "http://www.nytimes.com/" + doc.multimedia[0].url;
+          newEasyImageQuestion.answerChoices.push({'value': doc.headline.main, 'isCorrect': true});
+          newEasyImageQuestion.questionType = "image";
+          newEasyImageQuestion.showQuestion = true;
+          newEasyImageQuestion.difficultyLevel = 0;
+
+          for (var i = 0; i < 4; i++) {
+            var randomIndex = Math.floor((Math.random() * (docsArray.length -1)) + 0);
+            if (newEasyImageQuestion.answerChoices[0].value !== $scope.articleTitlesArray[randomIndex]) {
+              newEasyImageQuestion.answerChoices.push({'value': $scope.articleTitlesArray[randomIndex], 'isCorrect': false});
+            }
+          }
+          $scope.imageQuestions.push(newEasyImageQuestion);
+        }
+
+
+        // medium
+        if (doc.multimedia.length > 0 && doc.keywords.length > 0) {
+          var newMediumImageQuestion = new ImageQuestion();
+          newMediumImageQuestion.question = "The image above relates most to which set of keywords?";
+          newMediumImageQuestion.image = "http://www.nytimes.com/" + doc.multimedia[0].url;
+          newMediumImageQuestion.questionType = "image";
+          newMediumImageQuestion.showQuestion = true;
+          newMediumImageQuestion.difficultyLevel = 1;
+          var keywordsString = "";
+          doc.keywords.forEach(function(keywordObject) {
+            if (keywordObject.value) {
+              keywordsString += keywordObject.value + "," + " ";
+            }
+          });
+          keywordsString = keywordsString.trim();
+          keywordsString = keywordsString.substring(0, keywordsString.length-1);
+          newMediumImageQuestion.answerChoices.push({'value': keywordsString, 'isCorrect': true});
+
+          for (var i = 0; i < 4; i++) {
+            var randomIndex = Math.floor((Math.random() * (docsArray.length -1)) + 0);
+            if (newMediumImageQuestion.answerChoices[0].value !== $scope.articleKeyWordsArray[randomIndex]) {
+              newMediumImageQuestion.answerChoices.push({'value': $scope.articleKeyWordsArray[randomIndex], 'isCorrect': false});
+            }
+          }
+          $scope.imageQuestions.push(newMediumImageQuestion);
+
+        }
+
+        // hard
+        if (doc.snippet && doc.multimedia.length > 0) {
+          var newHardImageQuestion = new ImageQuestion();
+          newHardImageQuestion.question = "Which image does the quotation above belong to?";
+          newHardImageQuestion['snippet'] = doc.snippet;
+          newHardImageQuestion.image = "http://www.nytimes.com/" + doc.multimedia[0].url;
+          newHardImageQuestion.questionType = "image";
+          newHardImageQuestion.showQuestion = true;
+          newHardImageQuestion.difficultyLevel = 2;
+          $scope.imageQuestions.push(newHardImageQuestion);
+          newHardImageQuestion.answerChoices.push({'value': newHardImageQuestion.image, 'isCorrect': true});
+
+          for (var i = 0; i < 4; i++) {
+            var randomIndex = Math.floor((Math.random() * (docsArray.length -1)) + 0);
+            if (newHardImageQuestion.answerChoices[0].value !== $scope.articleImagesArray[randomIndex]) {
+              debugger;
+              newHardImageQuestion.answerChoices.push({'value': $scope.articleImagesArray[randomIndex], 'isCorrect': false});
+            }
+          }
+        }
+
+
+      });
 
 
 
-                // generate sentence finishers
-                var sentenceFinisherQuestionPreArray = [];
-                docsArray.forEach(function(document) {
-                  if ( document.snippet && document.snippet.match( /[A-Z]([a-z]+|\.)(?:\s+[A-Z]([a-z]+|\.))*(?:\s+[a-z][a-z\-]+){0,2}\s+[A-Z]([a-z]+|\.)/ ))  {
-                    var matchedSnippet = document.snippet.match(/[A-Z]([a-z]+|\.)(?:\s+[A-Z]([a-z]+|\.))*(?:\s+[a-z][a-z\-]+){0,2}\s+[A-Z]([a-z]+|\.)/);
-                    var matchedSnippetPostString = document.snippet.substring(matchedSnippet[0].length, document.snippet.length);
-                    var newSentenceFinisherQuestion = new SentenceFinisherQuestion();
-                    // question.should be the matched spippet
-                    newSentenceFinisherQuestion.question = matchedSnippet[0] + "...";
-                    newSentenceFinisherQuestion.answerChoices.push({'value': matchedSnippetPostString + "...", isCorrect: true});
-                    newSentenceFinisherQuestion.questionType = "SentenceFinisherQuestion";
-                    newSentenceFinisherQuestion.showQuestion = false;
-                    $scope.sentenceFinisherQuestionAnswersArray.push(matchedSnippetPostString);
-                    sentenceFinisherQuestionPreArray.push(newSentenceFinisherQuestion);
-                  }
-                });
-
-                sentenceFinisherQuestionPreArray.forEach(function(question) {
-                  for (var i = 0; i < 4; i++) {
-                    var randomIndex = Math.floor((Math.random() * ($scope.sentenceFinisherQuestionAnswersArray.length -1)) + 0);
-                    if (question.answerChoices[0].value !== $scope.sentenceFinisherQuestionAnswersArray[randomIndex]) {
-                      question.answerChoices.push({'value': $scope.sentenceFinisherQuestionAnswersArray[randomIndex], 'isCorrect': false});
-                    }
-                  }
-                  $scope.sentenceFinisherQuestions.push(question);
-                });
-
-
-              });
-            };
 
 
 
 
 
-    var queryApi = function() {
-      var keepLookingThoughTheApi = true;
-      var pageNumber = 0;
-      while(keepLookingThoughTheApi) {
-        setTimeout($scope.apiCallNYT(pageNumber), 10000);
-        pageNumber++;
-        if (pageNumber === 2) keepLookingThoughTheApi = false;
-      }
+      // outside of all for loops
+      $scope.quizObject.questions = $scope.imageQuestions;
+      console.log($scope.articleKeyWordsArray);
+      console.log($scope.quizObject);
+      console.log($scope.articleSnippetsArray);
+      console.log($scope.imageQuestions);
+
+
+
+
+    });
+  };
+
+
+
+  var queryApi = function() {
+    var keepLookingThoughTheApi = true;
+    var pageNumber = 0;
+    while(keepLookingThoughTheApi) {
+      setTimeout($scope.apiCallNYT(pageNumber), 10000);
+      pageNumber++;
+      if (pageNumber === 2) keepLookingThoughTheApi = false;
     }
-    queryApi();
+  }
+  queryApi();
 
-  });
+
+
+
+  $scope.easyQuestionFilter = function(question) {
+    if (question.difficultyLevel === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  $scope.mediumQuestionFilter = function(question) {
+    if (question.difficultyLevel === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  $scope.hardQuestionFilter = function(question) {
+    if (question.difficultyLevel === 2) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+});
